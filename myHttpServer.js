422aa93caa9e62bcca505abe548386b4e1fff376
liu <1812896452@qq.com>
2021-04-20 23:21:16 -0700

    0421 changes

--- myHttpServer.js
+++ myHttpServer.js
@@ -33,17 +33,39 @@ const server=http.createServer((req,res)=>{
     else if(req.url.slice(0,6)=="/input"){
       res.statusCode = 200;
       let url1=req.url.split("?");
-
-      // let urlquery=url1[1].split("&");
+      //console.log(url1[1])
+      //let urlquery=url1[1].split("&");     
+      //let firstQuery=urlquery[0].split("=");
+      //let secondQuery=urlquery[1].split("=");
+      let obQuery=querystring.parse(url1[1]);
+      //url1=new URL(req,url)
+      //let queryData=url.parse(req.url,true).query
+      if(obQuery.submit1=="Save"){
+        fs.writeFile('./savefile',obQuery.name1,(err)=>{
+          if(err) console.log("write file error")
+          else console.log("write file success")
+        })
+      }
+      else{
+          fs.appendFile('./savefile',obQuery.name1,(err)=>{
+          if(err) console.log("write file error")
+          else console.log("append file success")
+        })
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      fs.readFile("form3.html",(err,fsData)=>{
+        if(err){
+          console.log("Read file error")
+          throw err
+        }
+        res.write(fsData)
+        res.end()
+      })
       
-      // let firstQuery=urlquery[0].split("=");
-      // let secondQuery=urlquery[1].split("=");
-      let obQuery=querystring.parse(url[1]);
-
       res.setHeader('Content-Type','text/html');
-      res.write(obQuery.name+"<br>");
-      res.write(obQuery.submit+"<br>");
-
+      res.write(obQuery.name1+"<br>");
+      res.write(obQuery.submit1+"<br>");
       res.end("submit success!")
   }
     else{
@@ -51,7 +73,7 @@ const server=http.createServer((req,res)=>{
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
       //res.write(req.url)
-      res.write('<h1>This is Liu. You are the '+i+'th dog</h1>');
+      res.write('<h1>This is Liu. You are the '+i+'th sb</h1>');
       res.end();
     }
     //i++;
